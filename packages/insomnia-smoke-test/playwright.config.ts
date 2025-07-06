7fb20158905ad4b1b1437d727287832dbe2e9d39
Qi <gong_qi@qq.com>
2025-07-06 19:08:56 +0800

    Add html report for local run

--- packages/insomnia-smoke-test/playwright.config.ts
+++ packages/insomnia-smoke-test/playwright.config.ts
@@ -34,7 +34,7 @@ const config: PlaywrightTestConfig = {
       sources: true,
     },
   },
-  reporter: process.env.CI ? [['github'], ['line']] : [['list']],
+  reporter: process.env.CI ? [['github'], ['line']] : [['list'], ['html', { outputFolder: 'playwright-report' }]],
   timeout: process.env.CI ? 60 * 1000 : 20 * 1000,
   forbidOnly: !!process.env.CI,
   outputDir: 'traces',
